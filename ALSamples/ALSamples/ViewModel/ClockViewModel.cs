2fdec36428c78ddda05eeed508749ab0cb34f665
Shafiq_FlexiTeam <Shafiq_FlexiTeam@FLEXITEAM-PC>
2016-07-22 16:36:25 +0800

    Add project files.

---
+++ ALSamples/ALSamples/ViewModel/ClockViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ALSamples
+{
+    class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+
+        //private void NotifyPropertyChanged(string info)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("sini berubah"));
+        //}
+    }
+}
